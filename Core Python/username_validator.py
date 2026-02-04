70c3e220db2fd73cf196dc448f613662d065b387
tanishq <ponasanthem@gmail.com>
2026-02-04 23:36:21 +0530

    Added code for username checker

--- Core Python/username_validator.py
+++ Core Python/username_validator.py
@@ -1,4 +1,23 @@
+"""
+Rules:
+
+ -> Length: 5–15
+ -> Only letters, digits, underscore
+ -> Cannot start with digit
+"""
 def username_validator(username):
-    if 5 <= len(username) <= 15:
+    if len(username) < 5 or len(username) > 15:
         return False
     
+    if not username[0].isalpha():
+        return False
+    
+    for ch in username:
+        if not(ch.isalnum() or ch == '_'):
+            return False
+    
+    return True
+
+username = input("Enter a username: ")
+result  = username_validator(username)
+print(result)
